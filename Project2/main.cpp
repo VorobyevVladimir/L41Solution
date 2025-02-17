b6e69d1c7f60f4f1096c6ce4dc3487f190833e18
VorobyevVladimir <vladimirv15012010@gmail.com>
2025-02-17 17:59:56 +0300

    add matrix

---
+++ Project2/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+#define N 5
+#define M 10
+
+int main() {
+
+	int matrix[N][N]{};
+	int sum = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++) {
+			int number = rand() % 101;
+
+			cout << number << "  ";
+
+			if (i == j) {
+				sum += number;
+			}
+
+		}
+		cout << endl;
+	}
+	cout  << sum << endl;
+
+	return 0;
+}
+
